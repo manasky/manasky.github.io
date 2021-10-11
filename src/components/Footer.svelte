f0fee23d3465ce5de0d3c384b7b407b22ddeefd5
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-10-11 03:32:43 +0300

    fix footer icon size

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,12 +3,12 @@
   <div class="flex">
     <span class="tooltip tooltip-secondary" data-tip="GitHub">
       <a href="https://github.com/manasky" class="btn btn-sm btn-circle btn-ghost">
-        <img class="w-6 h-6" src="/icons/github.svg" alt="GitHub"/>
+        <img width="24" height="24" class="w-6 h-6" src="/icons/github.svg" alt="GitHub"/>
       </a>
     </span>
     <span class="tooltip tooltip-secondary" data-tip="Mail">
       <a href="mailto:hi@manasky.com" class="btn btn-sm btn-circle btn-ghost">
-        <img class="w-6 h-6" src="/icons/mail.svg" alt="Mail"/>
+        <img width="24" height="24" class="w-6 h-6" src="/icons/mail.svg" alt="Mail"/>
       </a>
     </span>
   </div>
